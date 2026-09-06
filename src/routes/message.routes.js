fd75c7802375d6db79f6fef8162388fe396ac3ff
Aditya Raj Mishra <adityarajmishra807@gmail.com>
2026-09-06 18:59:49 +0530

    Add message route rate limits

--- src/routes/message.routes.js
+++ src/routes/message.routes.js
@@ -3,6 +3,7 @@ const multer = require('multer');
 const ctrl = require('../controllers/message.controller');
 const asyncHandler = require('../middleware/asyncHandler');
 const env = require('../config/env');
+const { sendLimiter } = require('../middleware/rateLimiters');
 
 const upload = multer({
   storage: multer.memoryStorage(),
@@ -10,7 +11,7 @@ const upload = multer({
 });
 
 router.get('/:sessionId/:jid/history', asyncHandler(ctrl.history));
-router.post('/:sessionId/:jid/send', upload.single('file'), asyncHandler(ctrl.send));
+router.post('/:sessionId/:jid/send', sendLimiter, upload.single('file'), asyncHandler(ctrl.send));
 router.post('/:sessionId/:jid/read', asyncHandler(ctrl.markRead));
 router.patch('/:sessionId/:jid/:messageId', asyncHandler(ctrl.edit));
 router.delete('/:sessionId/:jid/:messageId', asyncHandler(ctrl.remove));
